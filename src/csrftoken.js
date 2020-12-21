0bf087d961642b38f96724887fee027ae1b39807
margap1998 <margap55@gmail.com>
2020-12-21 13:26:22 +0100

    small changes Signed-off-by: margap1998 <margap55@gmail.com>

--- src/csrftoken.js
+++ src/csrftoken.js
@@ -1,6 +1,12 @@
+import axios from "axios";
+
 function getCookie(cname) {
+    axios.defaults.xsrfCookieName = 'csrftoken'
+    axios.defaults.xsrfHeaderName = "X-CSRFToken"
+
     var name = cname + "=";
     var decodedCookie = decodeURIComponent(document.cookie);
+    console.log(decodedCookie)
     var ca = decodedCookie.split(';');
     for(var i = 0; i <ca.length; i++) {
       var c = ca[i];
@@ -15,6 +21,8 @@ function getCookie(cname) {
   }
 
 function getCSRFToken (){
-    return getCookie('csrftoken');
-};
+  let res = getCookie('csrftoken');
+  return res
+}
+
 export {getCSRFToken};
